9337f4bf85a3d66cd08a0e948a715ed5d742fcff
jakehayduk <j_hayduk@live.concordia.ca>
2025-11-24 00:52:02 -0500

    Survey has been completed

---
+++ final-project/script.js
@@ -0,0 +1,143 @@
+$(document).ready(function() {
+
+    let answers = []
+    let question = 0;
+
+    let survey = ["What is your name?", "What city were you born in?", "How old are you?", "When looking for a new product or service, what is your primary decision-making criteria?", "What is your childhood friend's name?", "If you were guaranteed to succeed, what is one major life goal you would pursue in the next 12 months?"]
+
+    $('.start-button').on('click', function() {
+        $('.start-screen').hide();
+        $('.survey').css('display', 'flex');
+        
+        $('.question').text(survey[question]);
+        question++;
+    })
+
+    $('.next-button').on('click', function() {
+        next();
+    })
+
+    $(document).on('keypress',function(e) {
+        if(e.which == 13) {
+            next();
+        }
+    });
+
+    function next() {
+        if ($('.answer').val().length > 1 && question < 7) {
+            $('.question').text(survey[question]);
+            answers.push($('.answer').val())
+            $('.answer').val('');
+            question++;
+        }
+        if (question > 6) {
+            $('.survey').hide();
+            $('.end-screen').css('display', 'flex');
+            $('.data-1').text(answers[0]);
+            $('.data-2').text(answers[1]);
+            $('.data-3').text(answers[2]);
+            $('.data-4').text(answers[3]);
+            $('.data-5').text(answers[4]);
+            $('.data-6').text(answers[5]);
+            $('.cube').css('animation', 'cube-in 1s')
+        }
+    }
+
+    $('.nav-button').on('click', function() {
+        $('.modal').css('display', 'grid');
+    })
+    
+    $('.modal-close').on('click', function() {
+        $('.modal').hide();
+    })
+    
+    // CUBE
+
+    let initialX = -20;
+    let initialY = -20;
+    let x = initialX;
+    let y = initialY;
+    let x2 = initialX;
+    let y2 = initialY;
+    let x3 = initialX;
+    let y3 = initialY;
+    let xSave = initialX;
+    let ySave = initialY;
+    let dragging = false;
+    let zoom = 1.4;
+
+    $(document).on('touchstart', function(e) {
+        dragging = true;
+        xSave = e.touches[0].clientX;
+        ySave = e.touches[0].clientY;
+    })
+
+    $(document).on('touchend', function() {
+        dragging = false;
+        x2 = x;
+        y2 = y;
+    })
+
+    $(document).on('touchmove', function(e) {
+        if (dragging == true) {
+            // divide by zoom variable instead of a static number like 10 so that the cube rotates faster when zoomed out and slower when zoomed in.
+            x = ((e.touches[0].clientX - xSave)/(zoom*3)) + x2;
+            y = -((e.touches[0].clientY - ySave)/(zoom*3)) + y2;
+        }
+    })
+
+    $(document).mousedown(function(e) {
+        dragging = true;
+        xSave = e.pageX;
+        ySave = e.pageY;
+    })
+
+    $(document).mouseup(function() {
+        dragging = false;
+        x2 = x;
+        y2 = y;
+    })
+
+    $(document).on('mousemove', function(e) {
+        if (dragging == true) {
+            // divide by zoom variable instead of a static number like 10 so that the cube rotates faster when zoomed out and slower when zoomed in.
+            x = ((e.pageX - xSave)/(zoom*5)) + x2;
+            y = -((e.pageY - ySave)/(zoom*5)) + y2;
+        }
+    })
+
+    setInterval(function() {
+        x3 = lerp(x3, x, 0.03);
+        y3 = lerp(y3, y, 0.03);
+
+        $('.cube').css('transform', 'rotateY(' + x3 + 'deg)');
+        $('.cube').css('rotate', 'x ' + y3 + 'deg');
+    
+    }, 1)
+
+    function lerp(a, b, n) {
+        return (1 - n) * a + n * b;
+    }
+
+    $(window).bind('mousewheel', function(event) {
+        if (zoom <= 1.4 && zoom >= 0.5) {
+            if (event.originalEvent.wheelDelta >= 0) {
+                zoom = zoom + 0.2;
+                $('.container').css('transform', 'scale(' + zoom + ')');
+            }
+    
+            else {
+                zoom = zoom - 0.2;
+                $('.container').css('transform', 'scale(' + zoom + ')');
+            }
+        }
+
+        else if (zoom > 1.4) {
+            zoom = 1.4;
+        }
+
+        else if (zoom < 0.5) {
+            zoom = 0.5;
+        }
+    });
+})
